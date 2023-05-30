ad97b16726c0d426e2735a7b9fa63ba36bc5f100
nils <48135649+Nilstrieb@users.noreply.github.com>
2023-05-30 15:51:17 +0200

    more

--- check.sh
+++ check.sh
@@ -1,12 +1,12 @@
 #/usr/bin/env bash
 
-rustc code.rs --crate-name bad -Zmir-enable-passes=-ConstProp
-rustc code.rs --crate-name good -Zmir-enable-passes=+ConstProp
+rustc code.rs --crate-name bad -Zmir-enable-passes=-ConstProp -Copt-level=3
+rustc code.rs --crate-name good -Zmir-enable-passes=+ConstProp -Copt-level=3
 
 bad=$(./bad)
 good=$(./good)
 
-if [ good != bad ]; then
+if [ "$good" != "$bad" ]; then
     echo "MISCOMPILATION"
 else
     echo "no repro"
