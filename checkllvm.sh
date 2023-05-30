aa0f9c35538eba9bf0844870ddf4305b6758d501
nils <48135649+Nilstrieb@users.noreply.github.com>
2023-05-30 16:46:04 +0200

    more

--- checkllvm.sh
+++ checkllvm.sh
@@ -2,6 +2,14 @@
 
 rustc code.rs --crate-name ll -Zmir-enable-passes=-ConstProp --emit llvm-ir -Cno-prepopulate-passes --crate-type=lib
 
-clang ll.ll helper.c
+clang ll.ll helper.c -O1 -o good
+clang ll.ll helper.c -O2 -o bad
 
-./a.out
+bad=$(./bad)
+good=$(./good)
+
+if [ "$good" != "$bad" ]; then
+    echo "MISCOMPILATION"
+else
+    echo "no repro"
+fi
